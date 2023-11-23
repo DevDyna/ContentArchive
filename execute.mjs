8c158f5a79e8e70de9216fec05268532ac14824a
DevDyna <andrearisorti2003@gmail.com>
2023-11-23 21:16:10 +0100

    shell to generate readme

---
+++ execute.mjs
@@ -0,0 +1,31 @@
+import * as io from './.methods/io.mjs';
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+//await delay(1000) /// waiting 1 second.	///only if async enable
+
+
+main()
+
+async function main() {
+    let readme = './README.md'
+        await delay(100)
+        console.log('Reading directories')
+        await delay(100)
+        let list = io.getAllFromDir('../ContentArchive/')
+        let filtered = list.filter(item => !(item.startsWith('.') || item.endsWith('.md') || item.endsWith('.mjs') || item.endsWith('.bat') || item.endsWith('.ini')));
+    await delay(100)
+    let plus = ''
+        if (filtered.length > 1) {
+            plus = 's'
+        }
+        console.log(filtered.length + ' Project' + plus + ' founded')
+        io.rawWrite(readme, '# ContentArchive\n')
+        io.rawAppend(readme, 'Archive for my public content creation\n')
+        io.rawAppend(readme, '### Projects related :\n')
+        await delay(100)
+        filtered.forEach(projects => {
+            io.rawAppend(readme, '`' + projects + '`\n')
+            console.log(projects + ' Added')
+        })
+
+}
